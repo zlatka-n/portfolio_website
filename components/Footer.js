cffecb0da34157cf1e0d47f6564b7feab0bf88c7
zlatka-n <zla.b.ngu@gmail.com>
2021-10-11 14:27:08 +0200

    footer finished

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,18 @@
+import { AiOutlineArrowUp } from "react-icons/ai";
+
 export default function Footer() {
   return (
-    <footer className="">
-      <div className="">Footer</div>
+    <footer className="max-w-7xl mx-auto py-10 lg:py-10 2xl:py-30 leading-normal md:text-xl">
+      <div className="flex justify-between">
+        <p className="">zlatka094@seznam.cz</p>
+        <div
+          className="flex items-center"
+          onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+        >
+          <AiOutlineArrowUp /> Top
+        </div>
+      </div>
+      <p>+420 735 204 082</p>
     </footer>
   );
 }
