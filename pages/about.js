b9b55fb7df2fdeea73308dd857dd33401e8b1073
zlatka-n <zla.b.ngu@gmail.com>
2021-10-12 11:22:49 +0200

    responsive tech stack

--- pages/about.js
+++ pages/about.js
@@ -25,17 +25,19 @@ export default function About() {
               Feel free to get in touch. I will try to read and respond to all
               emails.
             </p>
-            <h2 className=" w-2/6	md:w-1/5 my-10 p-2 box-content text-2xl md:text-3xl font-bold text-white bg-red-500 rounded">
+            <h2 className=" w-2/6	lg:w-1/5 my-10 p-2 box-content text-2xl md:text-3xl font-bold text-white bg-red-500 rounded">
               Tech stack
             </h2>
-            <div className="flex flex-col">
-              <div className="mx-auto">
-                <Image src="/techStack/combo.png" width={520} height={150} />
+            <div className="flex flex-col gap-y-5">
+              <div className="flex justify-center">
+                <Image src="/techStack/combo.png" width={500} height={120} />
               </div>
-              <div className="mx-auto flex">
-                <Image src="/techStack/react.png" width={150} height={120} />
-                <Image src="/techStack/redux.png" width={150} height={120} />
-                <Image src="/techStack/git3.png" width={170} height={120} />
+              <div className="flex justify-center lg:pl-20  md:gap-x-10">
+                <Image src="/techStack/react.png" width={140} height={110} />
+                <a className="">
+                  <Image src="/techStack/redux.png" width={140} height={110} />
+                </a>
+                <Image src="/techStack/git3.png" width={210} height={110} />
               </div>
             </div>
           </div>
