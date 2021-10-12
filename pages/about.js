e6a10768b514d6463b37da76f27c7e29e11df784
zlatka-n <zla.b.ngu@gmail.com>
2021-10-12 10:59:12 +0200

    rounded bg for tech stack h2

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,6 @@ export default function About() {
               Czech Republic. I’m currently working with Neit Consulting
               automating business processes.
             </p>
-            {/* <br /> */}
             <p className="text-xl my-10 leading-normal">
               I've started coding out of curiosity in June 2020 . At my current
               job, vanilla JavaScript is mostly needed, but I became curious
@@ -26,7 +25,9 @@ export default function About() {
               Feel free to get in touch. I will try to read and respond to all
               emails.
             </p>
-            <h2 className="text-2xl my-10 leading-normal">Tech stack</h2>
+            <h2 className=" w-2/6	md:w-1/5 my-10 p-2 box-content text-2xl md:text-3xl font-bold text-white bg-red-500 rounded">
+              Tech stack
+            </h2>
             <div className="flex flex-col">
               <div className="mx-auto">
                 <Image src="/techStack/combo.png" width={520} height={150} />
@@ -34,7 +35,7 @@ export default function About() {
               <div className="mx-auto flex">
                 <Image src="/techStack/react.png" width={150} height={120} />
                 <Image src="/techStack/redux.png" width={150} height={120} />
-                <Image src="/techStack/git3.png" width={160} height={120} />
+                <Image src="/techStack/git3.png" width={170} height={120} />
               </div>
             </div>
           </div>
