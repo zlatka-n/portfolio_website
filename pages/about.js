9c9b73b255496b5f2c68954681e0a84dfef2c2c2
zlatka-n <zla.b.ngu@gmail.com>
2021-10-13 07:51:09 +0200

    height changed

--- pages/about.js
+++ pages/about.js
@@ -25,19 +25,19 @@ export default function About() {
               Feel free to get in touch. I will try to read and respond to all
               emails.
             </p>
-            <h2 className=" w-2/6	lg:w-1/5 my-10 p-2 box-content text-2xl md:text-3xl font-bold text-white bg-red-500 rounded">
+            <h2 className=" w-2/6	lg:w-1/5 my-10 p-2 box-content text-2xl xl:text-3xl font-bold text-white bg-red-500 rounded">
               Tech stack
             </h2>
             <div className="flex flex-col gap-y-5">
               <div className="flex justify-center">
-                <Image src="/techStack/combo.png" width={500} height={120} />
+                <Image src="/techStack/combo.png" width={400} height={120} />
               </div>
               <div className="flex justify-center lg:pl-20  md:gap-x-10">
-                <Image src="/techStack/react.png" width={140} height={110} />
+                <Image src="/techStack/react.png" width={125} height={90} />
                 <a className="">
-                  <Image src="/techStack/redux.png" width={140} height={110} />
+                  <Image src="/techStack/redux.png" width={125} height={90} />
                 </a>
-                <Image src="/techStack/git3.png" width={210} height={110} />
+                <Image src="/techStack/git3.png" width={150} height={90} />
               </div>
             </div>
           </div>
