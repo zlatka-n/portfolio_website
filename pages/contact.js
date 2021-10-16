074b501dc001d846030df706fd14687fff6ac61f
zlatka-n <zla.b.ngu@gmail.com>
2021-10-16 20:50:28 +0200

    margins and paddings

--- pages/contact.js
+++ pages/contact.js
@@ -37,59 +37,70 @@ export default function ContactPage() {
     <Layout>
       <div className="mx-auto max-w-7xl">
         <div className="mx-auto md:w-3/5">
-          <h1 className="text-5xl md:text-7xl 4k:text-9xl font-bold px-5 md:px-0">
+          <h1 className="text-5xl md:text-7xl 4k:text-9xl font-bold px-5 md:px-0 pb-5">
             Contact
           </h1>
+          <p className="text-xl py-5">
+            Feel free to contact me also by phone +420 735 204 082. I will get
+            back to you as soon as I can.
+          </p>
           <form ref={form} onSubmit={handleSubmit(sendEmail)}>
-            <label>Full name</label>
-            <input
-              type="text"
-              name="user_name"
-              placeholder="Full name"
-              className="bg-gray-200 w-full h-14 px-3"
-              {...register("user_name", {
-                required: true,
-                maxLength: 20,
-              })}
-            />
-            {errors?.user_name?.type === "required" && (
-              <p className="text-red-500">This field is required</p>
-            )}
+            <div className="my-5">
+              <label className="text-xl">Full name</label>
+              <input
+                type="text"
+                name="user_name"
+                placeholder="Full name"
+                className="bg-gray-200 w-full h-14 px-3 my-2"
+                {...register("user_name", {
+                  required: true,
+                  maxLength: 20,
+                })}
+              />
+              {errors?.user_name?.type === "required" && (
+                <p className="text-red-500">This field is required</p>
+              )}
+            </div>
 
-            <label>Email</label>
-            <input
-              type="email"
-              name="user_email"
-              placeholder="Email"
-              className="bg-gray-200 w-full h-14 px-3"
-              {...register("user_email", {
-                required: true,
-                pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-              })}
-            />
-            {errors?.user_email?.type === "required" && (
-              <p className="text-red-500">This field is required</p>
-            )}
-            {errors?.user_email?.type === "pattern" && (
-              <p className="text-red-500">Invalid email</p>
-            )}
-            <label>Message</label>
-            <textarea
-              name="message"
-              className="bg-gray-200 w-full h-60 px-3"
-              placeholder="Message"
-              {...register("message", {
-                required: true,
-              })}
-            />
-            {errors?.message?.type === "required" && (
-              <p className="text-red-500">This field is required</p>
-            )}
-            <input
-              type="submit"
-              value="Send"
-              className="bg-black hover:bg-gray-500 text-white font-bold py-2 px-4 rounded"
-            />
+            <div className="my-5">
+              <label className="text-xl">Email</label>
+              <input
+                type="email"
+                name="user_email"
+                placeholder="Email"
+                className="bg-gray-200 w-full h-14 px-3 my-2"
+                {...register("user_email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                })}
+              />
+              {errors?.user_email?.type === "required" && (
+                <p className="text-red-500">This field is required</p>
+              )}
+              {errors?.user_email?.type === "pattern" && (
+                <p className="text-red-500">Invalid email</p>
+              )}
+            </div>
+
+            <div className="my-5">
+              <label className="text-xl">Message</label>
+              <textarea
+                name="message"
+                className="bg-gray-200 w-full h-60 px-3 my-2"
+                placeholder="Message"
+                {...register("message", {
+                  required: true,
+                })}
+              />
+              {errors?.message?.type === "required" && (
+                <p className="text-red-500">This field is required</p>
+              )}
+              <input
+                type="submit"
+                value="Send"
+                className="bg-black hover:bg-gray-500 text-white font-bold py-2 px-4 my-2 rounded"
+              />
+            </div>
           </form>
         </div>
       </div>
