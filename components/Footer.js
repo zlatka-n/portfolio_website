686513b84f7b737669330656f32f6671c87675b7
zlatka-n <zla.b.ngu@gmail.com>
2021-10-20 15:55:53 +0200

    px-5 for phones

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import { AiOutlineArrowUp } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <footer className="max-w-7xl mx-auto py-10 lg:py-10 2xl:py-30 px-2 md:px-5 xl:px-0 leading-normal md:text-xl">
+    <footer className="max-w-7xl mx-auto py-10 lg:py-10 2xl:py-30 px-2 px-5 xl:px-0 leading-normal md:text-xl">
       <div className="flex justify-between">
         <p className="">zlatka094@seznam.cz</p>
         <div
--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <header className="w-full py-10 lg:py-20 2xl:py-30 4k:py-40 ">
-      <div className="max-w-7xl	mx-auto px-2 md:px-5 xl:px-0 flex flex-col items-start md:flex-row md:justify-between md:text-xl">
+      <div className="max-w-7xl	mx-auto px-5 xl:px-0 flex flex-col items-start md:flex-row md:justify-between md:text-xl">
         <div className="py-5 md:py-0">Zlatka</div>
         <div className="space-x-5 md:space-x-10">
           <Link href="/">
