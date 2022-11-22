c7d31b67877b475f463fc73ee289e95595141b93
zlatka-n <zla.b.ngu@gmail.com>
2022-11-22 20:45:33 +0100

    change order for items in utils

---
+++ utils.js
@@ -0,0 +1,27 @@
+export const projectCardConfig = [
+  {
+    imgSrc: '/screens/bookstore1.png',
+    imgWidth: 814,
+    imgHeight: 460,
+    projectLink: 'https://eshop-redux-zlatka-n.vercel.app/',
+    githubLink: 'https://github.com/zlatka-n/eshop_redux',
+    name: 'Book Store'
+  },
+  {
+    imgSrc: '/screens/typicode1.png',
+    imgWidth: 709.67,
+    imgHeight: 401.04,
+    projectLink: 'https://typicode-json.vercel.app',
+    githubLink: 'https://github.com/zlatka-n/typicode_json',
+    name: 'JSON Placeholder'
+  },
+  {
+    imgSrc: '/screens/todo_redux2.png',
+    imgWidth: 514,
+    imgHeight: 460,
+    projectLink: 'https://todo-redux-liart.vercel.app/',
+    githubLink: 'https://github.com/zlatka-n/todo_redux/tree/master',
+    name: 'CRUD Todo List'
+  },
+
+];
