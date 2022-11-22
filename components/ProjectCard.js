763ee102795ce5c0f0f02ca877e6e3191f55f04d
zlatka-n <zla.b.ngu@gmail.com>
2022-11-22 20:17:37 +0100

    change html tages

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -8,14 +8,14 @@ export const ProjectCard = ({ imgSrc, imgWidth, imgHeight, projectLink, githubLi
             <h3 className="text-2xl py-5 text-gray-700 font-medium">Book Store</h3>
             <Image src={imgSrc} width={imgWidth} height={imgHeight} />
 
-            <div className=" my-5 flex gap-x-3">
+            <button className=" my-5 flex gap-x-3">
                 <a
                     href={githubLink}
                     target="_blank"
                     rel="noreferrer"
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 lg:mr-0 rounded"
                 >
-                    <AiFillGithub className="float-left text-xl" /> <span className="px-1">GitHub</span>
+                    <div className='flex'><AiFillGithub className="float-left text-xl" /> <h2 className="px-1">GitHub</h2></div>
                 </a>
                 <a
                     href={projectLink}
@@ -23,9 +23,9 @@ export const ProjectCard = ({ imgSrc, imgWidth, imgHeight, projectLink, githubLi
                     rel="noreferrer"
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
-                    <span className="px-5">Visit</span>
+                    <h2 className="px-5">Visit</h2>
                 </a>
-            </div>
+            </button>
         </div>
     );
 };
