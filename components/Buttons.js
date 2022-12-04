87302d3a74530263aaf35dc75818feb9d3280f3a
zlatka-n <zla.b.ngu@gmail.com>
2022-12-04 10:44:50 +0100

    create Buttons component

---
+++ components/Buttons.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { AiFillGithub } from 'react-icons/ai';
+
+export const Buttons = ({ githubLink, projectLink }) => {
+    return (
+        <div className=" my-5 flex gap-x-3">
+            <a
+                href={githubLink}
+                target="_blank"
+                rel="noreferrer"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mr-2 lg:mr-0 rounded">
+                <div className="flex">
+                    <AiFillGithub className="float-left text-xl" />
+                    <h2 className="px-1">GitHub</h2>
+                </div>
+            </a>
+            <a
+                href={projectLink}
+                target="_blank"
+                rel="noreferrer"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                <h2 className="px-5">Visit</h2>
+            </a>
+        </div>
+    )
+}
